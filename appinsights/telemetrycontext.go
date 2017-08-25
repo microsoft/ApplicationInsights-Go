eb7248b40716937b875f9e48abf896781c1fb34f
JJ Jordan <jjjordan@microsoft.com>
2017-08-25 01:10:28 -0700

    Fix generated telemetry context keys

--- appinsights/telemetrycontext.go
+++ appinsights/telemetrycontext.go
@@ -204,185 +204,185 @@ func (context *telemetryContext) setBoolTag(key string, value bool) {
 }
 
 func (context *applicationContext) GetVer() string {
-	return context.context.getStringTag(contracts.ApplicationVer)
+	return context.context.getStringTag("ai.application.ver")
 }
 
 func (context *applicationContext) SetVer(value string) {
-	context.context.setStringTag(contracts.ApplicationVer, value)
+	context.context.setStringTag("ai.application.ver", value)
 }
 
 func (context *deviceContext) GetId() string {
-	return context.context.getStringTag(contracts.DeviceId)
+	return context.context.getStringTag("ai.device.id")
 }
 
 func (context *deviceContext) SetId(value string) {
-	context.context.setStringTag(contracts.DeviceId, value)
+	context.context.setStringTag("ai.device.id", value)
 }
 
 func (context *deviceContext) GetLocale() string {
-	return context.context.getStringTag(contracts.DeviceLocale)
+	return context.context.getStringTag("ai.device.locale")
 }
 
 func (context *deviceContext) SetLocale(value string) {
-	context.context.setStringTag(contracts.DeviceLocale, value)
+	context.context.setStringTag("ai.device.locale", value)
 }
 
 func (context *deviceContext) GetModel() string {
-	return context.context.getStringTag(contracts.DeviceModel)
+	return context.context.getStringTag("ai.device.model")
 }
 
 func (context *deviceContext) SetModel(value string) {
-	context.context.setStringTag(contracts.DeviceModel, value)
+	context.context.setStringTag("ai.device.model", value)
 }
 
 func (context *deviceContext) GetOemName() string {
-	return context.context.getStringTag(contracts.DeviceOemName)
+	return context.context.getStringTag("ai.device.oemName")
 }
 
 func (context *deviceContext) SetOemName(value string) {
-	context.context.setStringTag(contracts.DeviceOemName, value)
+	context.context.setStringTag("ai.device.oemName", value)
 }
 
 func (context *deviceContext) GetOsVersion() string {
-	return context.context.getStringTag(contracts.DeviceOsVersion)
+	return context.context.getStringTag("ai.device.osVersion")
 }
 
 func (context *deviceContext) SetOsVersion(value string) {
-	context.context.setStringTag(contracts.DeviceOsVersion, value)
+	context.context.setStringTag("ai.device.osVersion", value)
 }
 
 func (context *deviceContext) GetType() string {
-	return context.context.getStringTag(contracts.DeviceType)
+	return context.context.getStringTag("ai.device.type")
 }
 
 func (context *deviceContext) SetType(value string) {
-	context.context.setStringTag(contracts.DeviceType, value)
+	context.context.setStringTag("ai.device.type", value)
 }
 
 func (context *locationContext) GetIp() string {
-	return context.context.getStringTag(contracts.LocationIp)
+	return context.context.getStringTag("ai.location.ip")
 }
 
 func (context *locationContext) SetIp(value string) {
-	context.context.setStringTag(contracts.LocationIp, value)
+	context.context.setStringTag("ai.location.ip", value)
 }
 
 func (context *operationContext) GetId() string {
-	return context.context.getStringTag(contracts.OperationId)
+	return context.context.getStringTag("ai.operation.id")
 }
 
 func (context *operationContext) SetId(value string) {
-	context.context.setStringTag(contracts.OperationId, value)
+	context.context.setStringTag("ai.operation.id", value)
 }
 
 func (context *operationContext) GetName() string {
-	return context.context.getStringTag(contracts.OperationName)
+	return context.context.getStringTag("ai.operation.name")
 }
 
 func (context *operationContext) SetName(value string) {
-	context.context.setStringTag(contracts.OperationName, value)
+	context.context.setStringTag("ai.operation.name", value)
 }
 
 func (context *operationContext) GetParentId() string {
-	return context.context.getStringTag(contracts.OperationParentId)
+	return context.context.getStringTag("ai.operation.parentId")
 }
 
 func (context *operationContext) SetParentId(value string) {
-	context.context.setStringTag(contracts.OperationParentId, value)
+	context.context.setStringTag("ai.operation.parentId", value)
 }
 
 func (context *operationContext) GetSyntheticSource() string {
-	return context.context.getStringTag(contracts.OperationSyntheticSource)
+	return context.context.getStringTag("ai.operation.syntheticSource")
 }
 
 func (context *operationContext) SetSyntheticSource(value string) {
-	context.context.setStringTag(contracts.OperationSyntheticSource, value)
+	context.context.setStringTag("ai.operation.syntheticSource", value)
 }
 
 func (context *operationContext) GetCorrelationVector() string {
-	return context.context.getStringTag(contracts.OperationCorrelationVector)
+	return context.context.getStringTag("ai.operation.correlationVector")
 }
 
 func (context *operationContext) SetCorrelationVector(value string) {
-	context.context.setStringTag(contracts.OperationCorrelationVector, value)
+	context.context.setStringTag("ai.operation.correlationVector", value)
 }
 
 func (context *sessionContext) GetId() string {
-	return context.context.getStringTag(contracts.SessionId)
+	return context.context.getStringTag("ai.session.id")
 }
 
 func (context *sessionContext) SetId(value string) {
-	context.context.setStringTag(contracts.SessionId, value)
+	context.context.setStringTag("ai.session.id", value)
 }
 
 func (context *sessionContext) GetIsFirst() string {
-	return context.context.getStringTag(contracts.SessionIsFirst)
+	return context.context.getStringTag("ai.session.isFirst")
 }
 
 func (context *sessionContext) SetIsFirst(value string) {
-	context.context.setStringTag(contracts.SessionIsFirst, value)
+	context.context.setStringTag("ai.session.isFirst", value)
 }
 
 func (context *userContext) GetAccountId() string {
-	return context.context.getStringTag(contracts.UserAccountId)
+	return context.context.getStringTag("ai.user.accountId")
 }
 
 func (context *userContext) SetAccountId(value string) {
-	context.context.setStringTag(contracts.UserAccountId, value)
+	context.context.setStringTag("ai.user.accountId", value)
 }
 
 func (context *userContext) GetId() string {
-	return context.context.getStringTag(contracts.UserId)
+	return context.context.getStringTag("ai.user.id")
 }
 
 func (context *userContext) SetId(value string) {
-	context.context.setStringTag(contracts.UserId, value)
+	context.context.setStringTag("ai.user.id", value)
 }
 
 func (context *userContext) GetAuthUserId() string {
-	return context.context.getStringTag(contracts.UserAuthUserId)
+	return context.context.getStringTag("ai.user.authUserId")
 }
 
 func (context *userContext) SetAuthUserId(value string) {
-	context.context.setStringTag(contracts.UserAuthUserId, value)
+	context.context.setStringTag("ai.user.authUserId", value)
 }
 
 func (context *cloudContext) GetRole() string {
-	return context.context.getStringTag(contracts.CloudRole)
+	return context.context.getStringTag("ai.cloud.role")
 }
 
 func (context *cloudContext) SetRole(value string) {
-	context.context.setStringTag(contracts.CloudRole, value)
+	context.context.setStringTag("ai.cloud.role", value)
 }
 
 func (context *cloudContext) GetRoleInstance() string {
-	return context.context.getStringTag(contracts.CloudRoleInstance)
+	return context.context.getStringTag("ai.cloud.roleInstance")
 }
 
 func (context *cloudContext) SetRoleInstance(value string) {
-	context.context.setStringTag(contracts.CloudRoleInstance, value)
+	context.context.setStringTag("ai.cloud.roleInstance", value)
 }
 
 func (context *internalContext) GetSdkVersion() string {
-	return context.context.getStringTag(contracts.InternalSdkVersion)
+	return context.context.getStringTag("ai.internal.sdkVersion")
 }
 
 func (context *internalContext) SetSdkVersion(value string) {
-	context.context.setStringTag(contracts.InternalSdkVersion, value)
+	context.context.setStringTag("ai.internal.sdkVersion", value)
 }
 
 func (context *internalContext) GetAgentVersion() string {
-	return context.context.getStringTag(contracts.InternalAgentVersion)
+	return context.context.getStringTag("ai.internal.agentVersion")
 }
 
 func (context *internalContext) SetAgentVersion(value string) {
-	context.context.setStringTag(contracts.InternalAgentVersion, value)
+	context.context.setStringTag("ai.internal.agentVersion", value)
 }
 
 func (context *internalContext) GetNodeName() string {
-	return context.context.getStringTag(contracts.InternalNodeName)
+	return context.context.getStringTag("ai.internal.nodeName")
 }
 
 func (context *internalContext) SetNodeName(value string) {
-	context.context.setStringTag(contracts.InternalNodeName, value)
+	context.context.setStringTag("ai.internal.nodeName", value)
 }
