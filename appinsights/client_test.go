c881e989a77721a9de6e6374c4efc8ac0cc1d161
JJ Jordan <jjjordan@microsoft.com>
2017-09-05 18:28:23 -0700

    Recast ClientBurstPerformance 'test' as a benchmark

--- appinsights/client_test.go
+++ appinsights/client_test.go
@@ -1,12 +1,17 @@
 package appinsights
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
-func TestClientBurstPerformance(t *testing.T) {
+func BenchmarkClientBurstPerformance(b *testing.B) {
 	client := NewTelemetryClient("")
 	client.(*telemetryClient).channel.(*InMemoryChannel).transmitter = &nullTransmitter{}
 
-	for i := 0; i < 1000000; i++ {
+	for i := 0; i < b.N; i++ {
 		client.TrackTrace("A message")
 	}
+
+	<-client.Channel().Close(time.Minute)
 }
